65764e416e2d556c0818ba3f0212310b1663040d
Michael Scragg <mike.scragg@gmail.com>
2015-07-12 14:34:22 -0400

    Performing MD5 checksum on script/css files and new option basePath

--- cli.js
+++ cli.js
@@ -3,24 +3,29 @@
 'use strict';
 
 var cachebust = require('./lib/cachebust'),
-fs = require('fs');
+  fs = require('fs'),
+  filePath;
 
-var userArgs = process.argv,
-filePath = userArgs[2];
+if (process.argv[2] === undefined) {
+  return console.log('Supply an html file as the first argument');
+}
+
+filePath = process.argv[2];
 
 if (filePath.indexOf('.html') > -1) {
   var options = {
-    type: (userArgs[3]) ? userArgs[3] : 'MD5'
+    basePath: 'test/fixtures/',
+    type: (process.argv[3]) ? process.argv[3] : 'MD5'
   };
 
   fs.readFile(filePath, 'utf8', function (err, html) {
     if (err) {
       throw err;
     }
-    cachebust.busted(html, options);
+    console.log(cachebust.busted(html, options));
   });
 }
 
-if (userArgs.indexOf('-v') !== -1 || userArgs.indexOf('--version') !== -1) {
+if (process.argv.indexOf('-v') !== -1 || process.argv.indexOf('--version') !== -1) {
   return console.log(require('./package').version);
 }
